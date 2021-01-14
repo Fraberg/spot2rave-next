ad2aa97855be7f4feaa0a26249233695f9c780d5
Fraberg <frcs.berger@gmail.com>
2021-01-15 00:56:23 +0100

    small fix

---
+++ back/public/precache-manifest.4eb202895645538cdeaf4fcc52e2bca5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f38af660f0f3ea4bee84",
+    "url": "/css/app.1f5a5262.css"
+  },
+  {
+    "revision": "580f7cf8f8c828729f1c30820b93f2a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f38af660f0f3ea4bee84",
+    "url": "/js/app.09e82831.js"
+  },
+  {
+    "revision": "05b3ace7cf7a4e62247c",
+    "url": "/js/chunk-vendors.b07131d5.js"
+  },
+  {
+    "revision": "2d80922148be7072d6f9c5c08325d80a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
