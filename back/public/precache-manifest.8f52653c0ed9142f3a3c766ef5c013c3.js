05a0a29dbd0d4cf4bee215cfb21fdc43e5c250fc
Fraberg <frcs.berger@gmail.com>
2021-01-15 02:32:36 +0100

    next step design events cards

---
+++ back/public/precache-manifest.8f52653c0ed9142f3a3c766ef5c013c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5143f51d2c641926975c",
+    "url": "/css/app.aeb313aa.css"
+  },
+  {
+    "revision": "08a74b39a9cdbb1c72842564a452e129",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5143f51d2c641926975c",
+    "url": "/js/app.5982fa8b.js"
+  },
+  {
+    "revision": "eed1308cc50497acd78b",
+    "url": "/js/chunk-vendors.507c4f99.js"
+  },
+  {
+    "revision": "2d80922148be7072d6f9c5c08325d80a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
